1b42968e135aea4a5138b23258298fe42096ddec
ajvcorreia <ajvcorreia@gmail.com>
2019-04-07 12:19:58 +0100

    Create table for Error in copies

--- db.py
+++ db.py
@@ -17,4 +17,5 @@ mydb = mysql.connector.connect(
               )
 mycursor = mydb.cursor()
 mycursor.execute("CREATE TABLE IF NOT EXISTS log (DateTime DATETIME, Message VARCHAR(255))")
-mycursor.execute("CREATE TABLE IF NOT EXISTS FilesCopied (DateTime DATETIME, Camera VARCHAR(255), Hash VARCHAR(255), FileName VARCHAR(255))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Files (DateTime DATETIME, Camera VARCHAR(255), Hash VARCHAR(255), FileName VARCHAR(255))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Errors (DateTime DATETIME, Camera VARCHAR(255), Hash VARCHAR(255), FileName VARCHAR(255), Error VARCHAR(255))")
