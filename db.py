57cd3e09de2691609a18da5b5625c8a9123da8f3
ajvcorreia <ajvcorreia@gmail.com>
2019-04-07 12:22:11 +0100

    fix name of log table

--- db.py
+++ db.py
@@ -16,6 +16,6 @@ mydb = mysql.connector.connect(
               database="photosynk"
               )
 mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE IF NOT EXISTS log (DateTime DATETIME, Message VARCHAR(255))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Log (DateTime DATETIME, Message VARCHAR(255))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS Files (DateTime DATETIME, Camera VARCHAR(255), Hash VARCHAR(255), FileName VARCHAR(255))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS Errors (DateTime DATETIME, Camera VARCHAR(255), Hash VARCHAR(255), FileName VARCHAR(255), Error VARCHAR(255))")
