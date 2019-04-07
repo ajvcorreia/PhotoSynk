00578bc88cadff3c0117a88e8e58b7e0c93ecc6b
ajvcorreia <ajvcorreia@gmail.com>
2019-04-07 12:07:47 +0100

    ...

--- db.py
+++ db.py
@@ -7,6 +7,12 @@ mydb = mysql.connector.connect(
               )
 
 mycursor = mydb.cursor()
-
 mycursor.execute("CREATE DATABASE IF NOT EXISTS photosynk")
-mycursor.execute("CREATE TABLE IF NOT EXISTS log (DateTime DATETIME, M:essage VARCHAR(255))")
+
+mydb = mysql.connector.connect(
+          host="localhost",
+            user="photosynk",
+              passwd="password"
+              database="photosynk"
+              )
+mycursor.execute("CREATE TABLE IF NOT EXISTS log (DateTime DATETIME, Message VARCHAR(255))")
